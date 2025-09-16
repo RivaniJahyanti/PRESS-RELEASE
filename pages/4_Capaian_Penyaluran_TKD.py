ad89c28e6e09bf8d3ddce49d976b40b6e759bf79
risaadytiasalma <salma.adytia20@gmail.com>
2025-09-16 18:07:33 +0700

    Update 4_Capaian_Penyaluran_TKD.py

--- pages/4_Capaian_Penyaluran_TKD.py
+++ pages/4_Capaian_Penyaluran_TKD.py
@@ -80,7 +80,7 @@ st.markdown("<h3 class='sub-header'>Lingkup KPPN Lhokseumawe</h3>", unsafe_allow
 
 # Periode/tanggal
 current_year = datetime.now().year
-st.markdown(f"<p class='period'>Periode: Januari - Juli {current_year}</p>", unsafe_allow_html=True)
+st.markdown(f"<p class='period'>Periode: Januari - Agustus {current_year}</p>", unsafe_allow_html=True)
 
 st.markdown('<hr class="divider">', unsafe_allow_html=True)
 
@@ -106,6 +106,6 @@ st.markdown('<hr class="divider">', unsafe_allow_html=True)
 # Catatan kaki
 st.caption("""
 **Catatan:**
-- **Sumber:** SINTESA 31 Juli 2025
-- Data yang disajikan adalah per tanggal 31 Juli 2025
+- **Sumber:** SINTESA 31 Agustus 2025
+- Data yang disajikan adalah per tanggal 31 Agustus 2025
 """)
