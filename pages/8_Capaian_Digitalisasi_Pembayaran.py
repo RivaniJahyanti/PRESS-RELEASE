17417bb71189640993946e814a9442e3f5e8e541
risaadytiasalma <salma.adytia20@gmail.com>
2025-10-16 07:00:48 +0700

    Update 8_Capaian_Digitalisasi_Pembayaran.py

--- pages/8_Capaian_Digitalisasi_Pembayaran.py
+++ pages/8_Capaian_Digitalisasi_Pembayaran.py
@@ -96,7 +96,7 @@ st.markdown("<h3 class='sub-header'>Lingkup KPPN Lhokseumawe</h3>", unsafe_allow
 
 # Periode/tanggal
 current_year = datetime.now().year
-st.markdown(f"<p class='period'>Periode: Januari - Agustus {current_year}</p>", unsafe_allow_html=True)
+st.markdown(f"<p class='period'>Periode: Januari - September {current_year}</p>", unsafe_allow_html=True)
 
 st.markdown('<hr class="divider">', unsafe_allow_html=True)
 
