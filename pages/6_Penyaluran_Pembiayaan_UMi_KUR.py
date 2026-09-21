e88886ef03f47c3509587493914e56a3a8fd3305
rekonkppn089-max <rekonkppn089@gmail.com>
2026-09-21 13:45:37 +0700

    Update 6_Penyaluran_Pembiayaan_UMi_KUR.py

--- pages/6_Penyaluran_Pembiayaan_UMi_KUR.py
+++ pages/6_Penyaluran_Pembiayaan_UMi_KUR.py
@@ -102,7 +102,7 @@ st.markdown("<h3 class='sub-header'>Lingkup KPPN Lhokseumawe</h3>", unsafe_allow
 
 # Periode/tanggal
 REPORT_YEAR = 2026
-st.markdown(f"<p class='period'>Periode: Januari - Juli {REPORT_YEAR}</p>", unsafe_allow_html=True)
+st.markdown(f"<p class='period'>Periode: Januari - Agustus {REPORT_YEAR}</p>", unsafe_allow_html=True)
 
 
 st.markdown('<hr class="divider">', unsafe_allow_html=True)
@@ -115,7 +115,7 @@ with st.container():
 
 # Analisis tambahan
 #with st.expander("**SUMBER:**", expanded=False):
-    #st.write("""SIKP UMi & KUR 3O Juni 2025""")
+    #st.write("""SIKP UMi & KUR 31 Agustus 2026""")
 
 # Tombol kembali ke home
 st.markdown('<div class="back-button">', unsafe_allow_html=True)
@@ -128,8 +128,8 @@ st.markdown('<hr class="divider">', unsafe_allow_html=True)
 # Catatan kaki
 st.caption("""
 **Catatan:**
-- **Sumber:** SIKP UMi & KUR 31 Juli 2026
-- Data yang disajikan adalah per tanggal 31 Juli 2026
+- **Sumber:** SIKP UMi & KUR 31 Agustus 2026
+- Data yang disajikan adalah per tanggal 31 Agustus 2026
 - UMi: Ultra Mikro
 - KUR: Kredit Usaha Rakyat
 """)
